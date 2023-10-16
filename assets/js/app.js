82af5638a7d537b8f360deadfd57a921c15dfd61
eri-maulana <maulanabouble11@gmail.com>
2023-10-16 10:57:58 +0700

    component finis

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,13 @@ M.Slider.init(slider, {
 
 
 const parallax = document.querySelectorAll(".parallax");
-M.Parallax.init(parallax)
+M.Parallax.init(parallax);
+
+const material = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(material);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+   scrollOffset : 50
+
+});
